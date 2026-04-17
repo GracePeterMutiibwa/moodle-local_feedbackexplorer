68c58f38ef9d262ac2433ed352309eb79a951d2c
Grace Peter Mutiibwa <gracepetermutiibwa@gmail.com>
2026-04-17 23:12:37 +0300

    Bump to v1.0 stable

--- version.php
+++ version.php
@@ -1,18 +1,10 @@
 <?php
-// Protect the file from being
-//  accessed directly via a web browser.
 defined('MOODLE_INTERNAL') || die();
 
-// The name of our plugin
-$plugin->component = 'local_feedbackexplorer'; 
-// The current date (YYYYMMDD) plus a 2-digit increment
-$plugin->version   = 2026041700; 
-// Requires Moodle 4.0 or newer              
-$plugin->requires  = 2022041900; 
- // We are in the testing phase              
-$plugin->maturity  = MATURITY_ALPHA;          
-$plugin->release   = 'v0.1';
-
-// Plugin metadata
+$plugin->component = 'local_feedbackexplorer';
+$plugin->version   = 2026041701;
+$plugin->requires  = 2022041900;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 'v1.0';
 $plugin->author    = 'Grace Peter Mutiibwa';
 $plugin->license   = 'GNU GPL v3 or later';
